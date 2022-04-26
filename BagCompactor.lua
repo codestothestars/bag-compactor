86f88e48bf87a46703a4d889a2d5ba7244dbf7e0
Ariel Oak <codestothestars@yahoo.com>
2022-04-25 21:05:03 -0400

    Refactor - more readable, flexible, concise, DRY

--- BagCompactor.lua
+++ BagCompactor.lua
@@ -1,3 +1,88 @@
+function Compact(arg)
+  local direction = GetDirection(arg)
+
+  local fromBag, fromSlot = GetStartSlot(direction)
+  local toBag, toSlot = GetStartSlot(-direction)
+
+  function Complete()
+    if fromBag == nil or toBag == nil then return true
+    elseif direction == ASCENDING then return fromBag > toBag or fromBag == toBag and fromSlot >= toSlot
+    else return fromBag < toBag or fromBag == toBag and fromSlot <= toSlot
+    end
+  end
+
+  while true do
+    fromBag, fromSlot = GetNextSlot(fromBag, fromSlot, toBag, direction, ValidFromSlot)
+
+    if Complete() then break end
+
+    toBag, toSlot = GetNextSlot(toBag, toSlot, fromBag, -direction, ValidToSlot)
+
+    if Complete() then break end
+
+    PickupContainerItem(fromBag, fromSlot)
+    PickupContainerItem(toBag, toSlot)
+  end
+end
+
+SLASH_COMPACT1 = "/compact"
+
+SlashCmdList["COMPACT"] = Compact
+
+ASCENDING = 1
+DESCENDING = -ASCENDING
+
+function GetDirection(arg)
+  if string.find(arg, 'back') then return ASCENDING else return DESCENDING end
+end
+
+function GetFirstSlot(bag, direction)
+  if direction == ASCENDING then return 1 else return GetContainerNumSlots(bag) end
+end
+
+function GetLastSlot(bag, direction)
+  if direction == ASCENDING then return GetContainerNumSlots(bag) else return 1 end
+end
+
+function GetNextSlot(fromBag, fromSlot, toBag, direction, filter)
+  for bag = fromBag, toBag, direction do
+    local _, special = GetContainerNumFreeSlots(bag)
+
+    if not special then
+      for slot = fromSlot + direction, GetLastSlot(bag, direction), direction do
+        local texture, _, locked = GetContainerItemInfo(bag, slot)
+
+        if filter(texture, locked) then return bag, slot end
+      end
+
+      fromSlot = GetFirstSlot(bag + direction, direction) - direction
+    end
+  end
+end
+
+function GetStartSlot(direction)
+  function GetBag()
+    if direction == ASCENDING then return 0 else return 4 end
+  end
+
+  local bag = GetBag()
+
+  function GetSlot()
+    if direction == ASCENDING then return 0 else return GetContainerNumSlots(bag) + 1 end
+  end
+
+  return bag, GetSlot()
+end
+
+function ValidFromSlot(texture, locked)
+  return texture and not locked
+end
+
+function ValidToSlot(texture)
+  return not texture
+end
+
+-- Polyfill GetContainerNumFreeSlots
 if GetContainerNumFreeSlots == nil then
   local specialBags = {
     'Big Bag of Enchantment',
@@ -10,7 +95,7 @@ if GetContainerNumFreeSlots == nil then
   }
 
   function GetInventoryItemName(slotID)
-    local link = GetInventoryItemLink('player', slotID)
+    local link = GetInventoryItemLink("player", slotID)
 
     return string.sub(link, string.find(link, '%[') + 1, string.find(link, ']') - 1)
   end
@@ -40,270 +125,6 @@ if GetContainerNumFreeSlots == nil then
       end
     end
 
-    return numFreeSlots, IsSpecialBag(bagID) and 1 or 0
+    return numFreeSlots, IsSpecialBag(bagID)
   end
 end
-
-function Compact(arg)
-  local currentBagFrom, currentSlotFrom, currentBagTo, currentSlotTo, numSlots, numFreeSlots, bagType
-
-  if arg == 'back' then
-    currentBagFrom, currentSlotFrom, currentBagTo, currentSlotTo = 0, 1, 4, 0
-  else
-    currentBagFrom, currentSlotFrom, currentBagTo, currentSlotTo = 4, 0, 0, 1
-  end
-
-  numSlots, numFreeSlots, specialBag = {}, {}, {}
-
-  --Loop that establishes the numbers of slots and free slots in each bag, as well as the bag type of each bag, to be kept track of for the duration of the function.
-  for i = 0, 4 do
-    local freeSlots, type = GetContainerNumFreeSlots(i)
-
-    numSlots[i] = GetContainerNumSlots(i)
-    numFreeSlots[i] = freeSlots
-    specialBag[i] = type > 0
-  end
-
-  if arg == 'back' then
-    currentSlotTo = numSlots[currentBagTo]
-  else
-    currentSlotFrom = numSlots[currentBagFrom]
-  end
-
-  --Primary loop that will terminate when currentSlotFrom and currentSlotTo have passed each other.
-  while true do
-    local compactionComplete = 0
-
-    --Select the proper currentBagFrom.
-    while true do
-      if numFreeSlots[currentBagFrom] == numSlots[currentBagFrom] or specialBag[currentBagFrom] or numSlots[currentBagFrom] == 0 then
-        if arg == 'back' then
-          currentBagFrom = currentBagFrom + 1
-          currentSlotFrom = 1
-
-          if currentBagFrom > currentBagTo then
-            compactionComplete = 1
-
-            break
-          end
-        else
-          currentBagFrom = currentBagFrom - 1
-          currentSlotFrom = numSlots[currentBagFrom]
-
-          if currentBagFrom < currentBagTo then
-            compactionComplete = 1
-
-            break
-          end
-        end
-      else
-        break
-      end
-    end
-
-    if compactionComplete == 1 then
-      break
-    end
-
-    --Select the proper currentSlotFrom.
-    while true do
-      local texture, _, locked = GetContainerItemInfo(currentBagFrom, currentSlotFrom)
-
-      if texture == nil or locked == 1 then
-        if arg == 'back' then
-          currentSlotFrom = currentSlotFrom + 1
-        else
-          currentSlotFrom = currentSlotFrom - 1
-        end
-
-        if locked == 1 then
-          numFreeSlots[currentBagFrom] = numFreeSlots[currentBagFrom] + 1
-        end
-
-        if arg == 'back' then
-          if currentBagFrom == currentBagTo and currentSlotFrom >= currentSlotTo then
-            compactionComplete = 1
-
-            break
-          end
-        else
-          if currentBagFrom == currentBagTo and currentSlotFrom <= currentSlotTo then
-            compactionComplete = 1
-
-            break
-          end
-        end
-
-        if arg == 'back' then
-          if currentSlotFrom > numSlots[currentBagFrom] then
-            while true do
-              if numFreeSlots[currentBagFrom] == numSlots[currentBagFrom] or specialBag[currentBagFrom] or numSlots[currentBagFrom] == 0 then
-                currentBagFrom = currentBagFrom + 1
-                currentSlotFrom = 1
-
-                if currentBagFrom > currentBagTo then
-                  compactionComplete = 1
-
-                  break
-                end
-              else
-                break
-              end
-            end
-
-            if compactionComplete == 1 then
-              break
-            end
-          end
-        else
-          if currentSlotFrom < 1 then
-            while true do
-              if numFreeSlots[currentBagFrom] == numSlots[currentBagFrom] or specialBag[currentBagFrom] or numSlots[currentBagFrom] == 0 then
-                currentBagFrom = currentBagFrom - 1
-                currentSlotFrom = numSlots[currentBagFrom]
-
-                if currentBagFrom < currentBagTo then
-                  compactionComplete = 1
-
-                  break
-                end
-              else
-                break
-              end
-            end
-            if compactionComplete == 1 then
-              break
-            end
-          end
-        end
-      else
-        break
-      end
-    end
-
-    if compactionComplete == 1 then
-      break
-    end
-
-    --Select the proper currentBagTo
-    while true do
-      if numFreeSlots[currentBagTo] == 0 or specialBag[currentBagTo] or numSlots[currentBagTo] == 0 then
-        if arg == 'back' then
-          currentBagTo = currentBagTo - 1
-          currentSlotTo = numSlots[currentBagTo]
-
-          if currentBagTo < currentBagFrom then
-            compactionComplete = 1
-
-            break
-          end
-        else
-          currentBagTo = currentBagTo + 1
-          currentSlotTo = 1
-
-          if currentBagTo > currentBagFrom then
-            compactionComplete = 1
-
-            break
-          end
-        end
-      else
-        break
-      end
-    end
-
-    if compactionComplete == 1 then
-      break
-    end
-
-    --Select the proper currentSlotTo
-    while true do
-      if GetContainerItemInfo(currentBagTo, currentSlotTo) == nil then
-        break
-      else
-        if arg == 'back' then
-          currentSlotTo = currentSlotTo - 1
-
-          if currentBagTo == currentBagFrom and currentSlotTo <= currentSlotFrom then
-            compactionComplete = 1
-
-            break
-          end
-
-          if currentSlotTo < 1 then
-            while true do
-              if numFreeSlots[currentBagTo] == 0 or specialBag[currentBagTo] then
-                currentBagTo = currentBagTo - 1
-                currentSlotTo = numSlots[currentBagTo]
-
-                if currentBagTo < currentBagFrom then
-                  compactionComplete = 1
-
-                  break
-                end
-              else
-                break
-              end
-            end
-
-            if compactionComplete == 1 then
-              break
-            end
-          end
-        else
-          currentSlotTo = currentSlotTo + 1
-
-          if currentBagTo == currentBagFrom and currentSlotTo >= currentSlotFrom then
-            compactionComplete = 1
-
-            break
-          end
-
-          if currentSlotTo > numSlots[currentBagTo] then
-            while true do
-              if numFreeSlots[currentBagTo] == 0 or specialBag[currentBagTo] then
-                currentBagTo = currentBagTo + 1
-                currentSlotTo = 1
-
-                if currentBagTo > currentBagFrom then
-                  compactionComplete = 1
-
-                  break
-                end
-              else
-                break
-              end
-            end
-
-            if compactionComplete == 1 then
-              break
-            end
-          end
-        end
-      end
-    end
-
-    if compactionComplete == 1 then
-      break
-    end
-
-    --Move the item from slot currentSlotFrom of bag currentBagFrom to slot currentSlotTo of bag currentBagTo.
-    PickupContainerItem(currentBagFrom, currentSlotFrom)
-    PickupContainerItem(currentBagTo, currentSlotTo)
-
-    numFreeSlots[currentBagFrom] = numFreeSlots[currentBagFrom] + 1
-    numFreeSlots[currentBagTo] = numFreeSlots[currentBagTo] - 1
-
-    if arg == 'back' then
-      currentSlotFrom = currentSlotFrom + 1
-      currentSlotTo = currentSlotTo - 1
-    else
-      currentSlotFrom = currentSlotFrom - 1
-      currentSlotTo = currentSlotTo + 1
-    end
-  end
-end
-
-SLASH_COMPACT1 = '/compact'
-
-SlashCmdList['COMPACT'] = Compact
